123c6aad309de16a7320b68f916feb65a58580cb
Mike404x <mmi6o88@gmail.com>
2024-04-20 12:31:00 +0300

    Update 05-special-numbers.js

--- 6-Exercise/05-special-numbers.js
+++ 6-Exercise/05-special-numbers.js
@@ -1,5 +1,5 @@
-function specialNumbers(params) {
-    let num = Number(params[0]);
+function specialNumbers(input) {
+    let num = Number(input[0]);
     let output = '';
     for (let i = 1; i <= 9; i++) {
         for (let j = 1; j <= 9; j++) {
